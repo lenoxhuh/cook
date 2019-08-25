bcb7c93852e85f11983933ebbfc464a841fb900a
lenoxhuh <lenoxhuh@gmail.com>
2019-08-25 15:48:59 -0700

    Getting started on home page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,13 +1,112 @@
 import React, { Component } from 'react';
+import { 
+  VictoryBar, 
+  VictoryChart, 
+  VictoryAxis, 
+  VictoryTheme,
+  VictoryLabel,
+  VictoryTooltip
+} from 'victory';
 import './Home.css';
 
-import Infrograph from '../Infrograph/index';
+import { 
+  InfroModule, 
+  Infrograph,
+  MealPlan
+} from '../Infrograph/index';
+
+var diabetes_data = [
+  {year: 2000, obesity: 30},
+  {year: 2001, obesity: 40 },
+  {year: 2002, obesity: 43},
+  {year: 2003, obesity: 40},
+  {year: 2004, obesity: 48},
+  {year: 2005, obesity: 50},
+  {year: 2006, obesity: 65},
+  {year: 2007, obesity: 70},
+];
+
+diabetes_data.forEach(datum => {
+  return datum['label'] = datum['obesity'] + "%";
+});
+
+const meal_plan_data = [
+  {category: "Carbohydrates", divisions: [
+    {type: "Simple", amount: 30},
+    {type: "Complex", amount: 50},
+    {type: "Sugars", amount: 20},
+  ]},
+  {category: "Protein", divisions: [
+    {type: "Meat", amount: 40},
+    {type: "Vegetables", amount: 40},
+    {type: "Other", amount: 20},
+  ]},
+  {category: "Vegetables", divisions: [
+    {type: "Roasted", amount: 20},
+    {type: "Salad", amount: 40},
+    {type: "Green Juice", amount: 40},
+  ]}
+]
 
 class Home extends Component {
 	render() {
 	  return (
 	  	<div className="App">
-        <Infrograph />
+        <Infrograph modules={[
+          (
+            <InfroModule
+              description={"Stats"}
+              graph={(
+                <VictoryChart
+                  domainPadding={20}
+                  theme={VictoryTheme.material}
+                  >
+                  <VictoryLabel 
+                    text={"Obesity Data"}
+                    textAnchor="middle"
+                    x={155} y={10}
+                    />
+                  <VictoryAxis
+                    independentAxis
+                    tickValues={diabetes_data.keys()}
+                    tickFormat={(x) => (`${x}`)}
+                    />
+                  <VictoryAxis
+                    dependentAxis
+                    tickFormat={(y) => (`${y}%`)}
+                    />
+                    <VictoryBar 
+                      data={diabetes_data}
+                      barWidth={15}
+                      style={{ 
+                          data: {
+                            position: "relative",
+                            top: "-5px",
+                            strokeLinejoin: "round",
+                            strokeWidth: 4,
+                            fill: "#64B963"
+                          }
+                      }}
+                      labelComponent={<VictoryTooltip
+                        />}
+                      x="year"
+                      y="obesity"
+                      />
+                 </VictoryChart>)}
+            />
+          ),
+          (
+            <InfroModule
+              description="Meal Plan"
+              graph={<MealPlan data={meal_plan_data} />}
+            />
+          ),
+          (   
+            <InfroModule
+              description="Exercise"
+            />
+          )
+        ]}/>
     	</div>
 	  );
 	}
--- client/src/components/Infrograph/index.js
+++ client/src/components/Infrograph/index.js
@@ -2,26 +2,115 @@ import React, { Component } from 'react';
 import Button from '../Button/index';
 import './Infrograph.css';
 
-// Carousel displays a card for each Nation
 class Infrograph extends Component {
 
-    render() {
-        return (
-          <div className="infrographic-wrapper">
-            <h1 className="text-shadow underline"> Millions of Americans are diagnosed as diabetic each year</h1>
-            <div className="desc-wrapper">
-              <p className="abstract"><i>abstract</i></p>
-              <p> The DRIF is focused on finding a way to beat diabetes & improve millions of lives. Relentless research and testing is our only focus. 
-            See how you can help today! The Best Hope for a Cure. Help Managing Diabetes. Research Updates & News. Be a Hero - Donate Now. Solely Focused on a Cure. </p>
-            </div>
-            <Button
-              title="Read More"
-              width="100px"
-              />
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: 0
+    }
+  }
+
+  get_modules() {
+    return this.props.modules;
+  }
+
+  render() {
+    return (
+      <div className="infrograph">
+        <div className="infrographic-wrapper">
+          <h1 className="text-shadow underline"> Millions of Americans are diagnosed as diabetic each year</h1>
+          <div className="desc-wrapper">
+            <p className="abstract"><i>abstract</i></p>
+            <p> The DRIF is focused on finding a way to beat diabetes & improve millions of lives. Relentless research and testing is our only focus. 
+          See how you can help today! The Best Hope for a Cure. Help Managing Diabetes. Research Updates & News. Be a Hero - Donate Now. Solely Focused on a Cure. </p>
           </div>
-        );
+          <Button
+            title="Read More"
+            width="100px"
+            />
+        </div>
+        <div className="infromodules-wrapper">
+            {this.get_modules()}
+        </div>
+      </div>
+      );
     }
 }
 
+class InfroModule extends Component {
 
-export default Infrograph;
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  
+  render() {
+    return (
+      <div className="module-wrapper-wrapper">
+        <div className="module-wrapper">
+          {this.props.graph}
+        </div>
+        <p className="module-description">
+          {this.props.description}
+        </p>
+      </div>
+    );
+  }
+
+}
+
+class MealPlan extends Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  get_plans() {
+    var plans = [];
+    if (!this.props.data) {
+      return plans;
+    }
+
+    this.props.data.forEach((data) => 
+      plans.push(
+        <MealPlanBar 
+          title={data.category}
+          data={data.divisions}
+          />
+      )
+    );
+    return plans;
+  }
+
+  render() {
+    return (
+      <div className="meal-plan-wrapper">
+        {this.get_plans()}
+      </div>
+    );
+  }
+}
+
+class MealPlanBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="meal-plan-bar">
+        <p className="meal-plan-title">
+          {this.props.title}     
+        </p>
+      </div>
+    );
+  }
+}
+
+export {
+  Infrograph,
+  InfroModule,
+  MealPlan
+}
